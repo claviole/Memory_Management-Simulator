98957550d98a14b47b29e3e65317f387e45116af
l4vav <l4vav@DESKTOP-E6TJB91>
2023-03-29 13:39:35 -0500

    Add project files.

---
+++ Laviolette_Project_2.cpp
@@ -0,0 +1,18 @@
+/*
+* Name : Christian Laviolette
+* Class:CS302
+* Project 2: Memory Management
+* Date: 3/29/23
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
---
+++ README.txt
@@ -0,0 +1,4 @@
+/*
+This program is a  project to be completed for CS309 to simulate Memory Management within an operating system.
+
+This project is due on 4/10/23
