5a1a8c66720d7c4983027c3130d9161eb00b7657
Christian Laviolette <claviole@pnw.edu>
2023-04-05 13:38:08 -0500

    Memory Management Project

--- Laviolette_Project_2.cpp
+++ Laviolette_Project_2.cpp
@@ -14,5 +14,99 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 #include <iterator>
+using namespace std;
 
+const int TLB_SIZE = 16;
+
+struct TLBEntry {
+    int pageNumber;
+    int frameNumber;
+};
+
+int main()
+{
+    int* pageTable = new int[256];
+    int* memory = new int[256 * 256];
+
+    for (int i = 0; i < 256; i++)
+    {
+        pageTable[i] = -1;
+    }
+
+    vector<TLBEntry> TLB(TLB_SIZE);
+    int nextFrame = 0;
+    int pageFaults = 0;
+    int pageHits = 0;
+    int TLBHits = 0;
+    int TLBMisses = 0;
+    int pagesTranslated = 0;
+
+
+    // open addresses.txt
+    ifstream inputFile("addresses.txt");
+    int virtualAddress;
+
+    while (inputFile >> virtualAddress)
+    {
+        signed char buffer[256];
+
+        int pageNumber = (virtualAddress >> 8) & 0xFF;
+        int offset = virtualAddress & 0xFF;
+        pagesTranslated++;
+
+        // Check TLB
+        bool TLBHit = false;
+        int frameNumber = -1;
+        for (int i = 0; i < TLB_SIZE; i++) {
+            if (TLB[i].pageNumber == pageNumber) {
+                frameNumber = TLB[i].frameNumber;
+                TLBHit = true;
+                break;
+            }
+        }
+
+        if (!TLBHit) {
+            // Check page table
+            if (pageTable[pageNumber] == -1) { // Page fault
+                ifstream input("BACKING_STORE.bin", ios::binary | ios::in);
+                input.seekg(pageNumber * 256, std::ios::beg);
+                input.read((char*)buffer, 256);
+                input.seekg(0, std::ios::beg);
+                input.close();
+
+                for (int i = 0; i < 256; i++)
+                {
+                    memory[nextFrame * 256 + i] = static_cast<int>(static_cast<signed char>(buffer[i]));
+                }
+                pageTable[pageNumber] = nextFrame;
+                nextFrame++;
+                pageFaults++;
+            }
+            frameNumber = pageTable[pageNumber];
+            // Update TLB
+            TLB[TLBMisses % TLB_SIZE].pageNumber = pageNumber;
+            TLB[TLBMisses % TLB_SIZE].frameNumber = frameNumber;
+            TLBMisses++;
+        }
+        else {
+            TLBHits++;
+        }
+
+        int physicalAddress = frameNumber * 256 + offset;
+
+        int data = memory[physicalAddress];
+        pageHits++;
+
+        cout << "Virtual address: " << virtualAddress << " Physical address: " << physicalAddress << " Value: " << data << endl;
+    }
+
+    cout << "Number of Translated Addresses = " << pagesTranslated << endl;
+    cout << "Page Faults= " << pageFaults << endl;
+    cout << "Page Fault Rate= " << (double)pageFaults / pagesTranslated << endl;
+    cout << "TLB Hits= " << TLBHits << endl;
+    cout << "TLB Hit Rate= " << (double)TLBHits / pagesTranslated << endl;
+
+    return 0;
+}
--- README.txt
+++ README.txt
@@ -1,4 +1,19 @@
 /*
 This program is a  project to be completed for CS309 to simulate Memory Management within an operating system.
 
+guidlines for the project:
+Read a file containg several 32 bit integer numbers that represent logical addresses
+However, only concerned with 16-bit address so we must mask the rightmost 16bits of each logical address
+The 16-bit address is then divided into 8-bit page number and 8-bit offset
+
+-2^8 entries in the page table
+-Page size of 2^8 bytes
+-16 entries in the TLB
+-frame size of 2^8 bytes
+-256 frames
+-Physical memory of 65,536 bytes(256 frames x 256 byte frame size)
+-Do not need to supportk wwriting to the logical address space
+
+
+-Program need only be concerned with reading logical addresses and translating them to their corresponding physical addresses
 This project is due on 4/10/23
